528a71a02ef330aeb89e0776db508ad57fa1bca3
MominaAmir <MominaAmir>
2023-11-25 23:48:09 +0500

    first cimmt

---
+++ index.js
@@ -0,0 +1,24 @@
+
+let sum = (a,b)=>{
+    return a+b
+}
+let sub = (a,b)=>{
+    return a-b
+}
+let div = (a,b)=>{
+    return a/b
+}
+let mult = (a,b)=>{
+    return a*b
+}
+let modulus = (a,b)=>{
+    return a%b
+}
+
+module.exports= {
+    sum:sum,
+    sub:sub,
+    div:div,
+    mult:mult,
+    modulus:modulus
+}
